3fd47ed3b58d95083024b5798b3f240b7262d46f
Lucas.Stecher <lucasvieira0070@gmail.com>
2021-05-09 10:10:54 -0300

    criação do index.html e add_user.html

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const morgan = require('morgan');
+const bodyparser = require("body-parser");
+const path = require('path');
+const { response } = require('express');
+
+const app = express();
+
+dotenv.config({path: 'config.env'})
+const PORT = process.env.PORT || 8080
+
+// log request
+app.use(morgan('tiny'))
+
+// parse request to body-parser
+app.use(bodyparser.urlencoded({ extented:true}))
+
+// set view engine
+app.set("view engine", "ejs")
+
+// load assets
+app.use('/css', express.static(path.resolve(__dirname, "assets/css")))
+app.use('/img', express.static(path.resolve(__dirname, "assets/ims")))
+app.use('/js', express.static(path.resolve(__dirname, "assets/js")))
+
+app.get('/', (req, res) =>{
+    res.render('index');
+})
+
+app.listen(PORT, () => {console.log(`Server is running on http://localhost:${PORT}`)});
