21840c27f1c7d90210a32818c1943c1769eafe43
Lucas.Stecher <lucasvieira0070@gmail.com>
2021-05-09 13:21:27 -0300

    configuração do banco de dados

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const dotenv = require('dotenv');
 const morgan = require('morgan');
 const bodyparser = require("body-parser");
 const path = require('path');
-const { response } = require('express');
+
+const connectDB = require('./server/database/connection');
 
 const app = express();
 
@@ -11,13 +12,17 @@ dotenv.config({path: 'config.env'})
 const PORT = process.env.PORT || 8080
 
 // log request
-app.use(morgan('tiny'))
+app.use(morgan('tiny'));
+
+//mongodb connection
+connectDB();
 
 // parse request to body-parser
 app.use(bodyparser.urlencoded({ extented:true}))
 
 // set view engine
 app.set("view engine", "ejs")
+//app.set("views", path.resolve(__dirname, "views/ejs"))
 
 // load assets
 app.use('/css', express.static(path.resolve(__dirname, "assets/css")))
